2654d3086596d47954eeb07e6e997640d31fb555
Cellenseres <github@cellenser.es>
2026-02-09 02:06:52 +0100

    CI: use ubuntu-24.04; add billboard header
    Update GitHub Actions to run on ubuntu-24.04 (including job name) and adjust pip invocation to use --break-system-packages for macholib. Add include/sssv_billboard_controls.h which declares a set of setter/getter functions for toggling billboard rendering, LOD, and various rewrite/suppress/log options used to control billboard rendering behavior.

---
+++ include/sssv_billboard_controls.h
@@ -0,0 +1,54 @@
+#pragma once
+
+namespace sssv::billboard {
+
+void set_disable_6fa3a4_render(bool enabled);
+bool get_disable_6fa3a4_render();
+
+void set_disable_lod(bool enabled);
+bool get_disable_lod();
+
+void set_disable_6c5e44_render(bool enabled);
+bool get_disable_6c5e44_render();
+
+void set_disable_73f17c_render(bool enabled);
+bool get_disable_73f17c_render();
+
+void set_disable_73f800_render(bool enabled);
+bool get_disable_73f800_render();
+
+void set_disable_740094_render(bool enabled);
+bool get_disable_740094_render();
+
+void set_disable_740820_render(bool enabled);
+bool get_disable_740820_render();
+
+void set_rewrite_6c5e44_ortho(bool enabled);
+bool get_rewrite_6c5e44_ortho();
+void set_rewrite_6c5e44_suppress_original(bool enabled);
+bool get_rewrite_6c5e44_suppress_original();
+
+void set_rewrite_73f17c_ortho(bool enabled);
+bool get_rewrite_73f17c_ortho();
+void set_rewrite_73f17c_suppress_original(bool enabled);
+bool get_rewrite_73f17c_suppress_original();
+
+void set_rewrite_73f800_ortho(bool enabled);
+bool get_rewrite_73f800_ortho();
+void set_rewrite_73f800_suppress_original(bool enabled);
+bool get_rewrite_73f800_suppress_original();
+
+void set_rewrite_740094_ortho(bool enabled);
+bool get_rewrite_740094_ortho();
+void set_rewrite_740094_suppress_original(bool enabled);
+bool get_rewrite_740094_suppress_original();
+
+void set_rewrite_740820_ortho(bool enabled);
+bool get_rewrite_740820_ortho();
+void set_rewrite_740820_suppress_original(bool enabled);
+bool get_rewrite_740820_suppress_original();
+
+void set_log_73f17c_ortho(bool enabled);
+bool get_log_73f17c_ortho();
+
+} // namespace sssv::billboard
